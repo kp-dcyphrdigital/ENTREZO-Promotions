0c0cbade0b892b7ee5aeed8336bd961f1abac9d1
Kazim P <kazim.parkar@syginteractive.com.au>
2018-04-24 16:22:58 +1000

    Banner CTA

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,12 +1,12 @@
-<section class="hero is-medium is-info is-bold">
+<a href="/enter"><section class="hero is-medium is-info is-bold">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
         Awesome Photo Competition
       </h1>
       <h2 class="subtitle">
-        Be in it to win it!
+        Be in it to win it, click to Enter Now!
       </h2>
     </div>
   </div>
-</section>
+</section></a>
