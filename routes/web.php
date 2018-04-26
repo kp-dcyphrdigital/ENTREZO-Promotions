c5a035fac4e3bf9a9e3b66d4c1cd302e28931b73
Kazim P <kazim.parkar@syginteractive.com.au>
2018-04-26 17:00:10 +1000

    Best to disable user registrations for now

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('admin/login', 'Auth\LoginController@login');
 Route::post('admin/logout', 'Auth\LoginController@logout')->name('logout');
 // Registration Routes...
-Route::get('admin/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('admin/register', 'Auth\RegisterController@register');
+// Route::get('admin/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// Route::post('admin/register', 'Auth\RegisterController@register');
 // Password Reset Routes...
 Route::get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
